cc83aa5400b9dc179e979805c2b1ed8859ee74e2
Alpa Patel <alpajp@live.com>
2018-12-21 21:45:08 -0500

    modified style,game,index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,48 @@
+var time = 60;
+var intervalId;
+var rightAns = ["a", "a", "b", "c", "b"];
+var correctAns = 0;
+var wrongAns = 0;
 
+timer();
 
-$("input[name='q1']:checked").val()
-$("input[name='q2']:checked").val()
-$("input[name='q3']:checked").val()
+function timer() {
+    clearInterval(intervalId);
+    setInterval(count, 1000);
+}
+
+function count() {
+    time--;
+    $("#timer").text(time);
+    if (time === 0) {
+        checkAns();
+    }
+}
+
+function checkAns() {
+    for (let i = 0; i < rightAns.length; i++) {
+        if ($(`input[name="q${i+1}"]:checked`).val() === rightAns[i]) {
+            correctAns++;
+        }
+        $("#correctAns").text(correctAns);
+        $("#wrongAns").text(rightAns.length - correctAns);
+        $("#timerDiv").hide(500);
+        $("form").hide(500);
+        $("#scoreBoard").show(500);
+    }
+}
+
+$("#submit").on("click", function (e) {
+    e.preventDefault();
+    checkAns();
+})
+
+var score = 0;
+var questionIndex = 0;
+
+function renderQuestion() {
+
+    if (questionIndex <= (questions.length - 1)) {
+        document.querySelector("#question").innerHTML = questions[questionIndex].q;
+    }
+}
